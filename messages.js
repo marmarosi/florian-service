0096c7b50bf343b75d626a4711307986b60fd683
Mármarosi József <marmarosi@logikum.hu>
2019-05-16 20:16:45 +0200

    Add command handlers.

---
+++ messages.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const messages = {
+
+  chars: {
+    arg: 'The movie title is missing.',
+    header: title => `The characters of the movie "${title}":`,
+  },
+
+  match: {
+    args: 'The movie titles are missing.',
+    arg2: 'The second movie title is missing.',
+    header: 'The characters that play in both movies:'
+  },
+
+  tall: {
+    arg: 'The required height of the characters is missing.',
+    header: height => `The characters that are at least ${height} cm tall:`
+  },
+
+  noMovie: title => `The movie "${title}" has not ben found.`,
+  underline: () => '='.repeat(40),
+
+  help: `
+Usage: node startwars.js <command> [...arguments]
+
+Where valid conmmands are the following:
+
+    characters <movie-title>
+        Returns all the characters of the movie.
+
+    matchingCharacters <movie-title-1> <movie-title-2>
+        Returns all characters that pay in both movies.
+
+    tall=<height-in-cm>
+        Returns all characters of all movies that have the given height at least.
+
+    help
+        Displays this list.`,
+
+  startup: port => `Example app listening on port ${port}!`,
+  hello: 'Hello World!'
+};
+
+module.exports = messages;
--- starwars.js
+++ starwars.js
@@ -1,7 +1,69 @@
+"use strict";
+
 const express = require('express');
+const api = require('./api.js');
+const msg = require('./messages.js');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const COMMAND = {
+  CHARS: 'characters',
+  MATCH: 'matchingCharacters',
+  TALL: 'tall='
+};
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const finish = () => {
+  server.close();
+};
+
+const display = message => {
+  console.log( message );
+  finish();
+};
+
+const server = app.listen( port, () => {
+
+  if (process.argv.length < 3) {
+    console.log( msg.startup( port ) );
+  }
+} );
+
+if (process.argv.length > 2) {
+
+  const cmd = process.argv[2];
+  switch (cmd) {
+
+    case COMMAND.CHARS:
+      if (process.argv.length < 4) {
+        display( msg.chars.arg );
+      } else {
+        api.getMovieCharacters( process.argv[3], finish );
+      }
+      break;
+
+    case COMMAND.MATCH:
+      if (process.argv.length < 4) {
+        display( msg.chars.args );
+      } else if (process.argv.length < 5) {
+        display( msg.chars.arg );
+    } else {
+      api.getMatchingCharacters( process.argv[3], process.argv[4], finish );
+      }
+      break;
+
+    default:
+      if (cmd.startsWith( COMMAND.TALL )) {
+        if (cmd.length > 5) {
+          api.getTallCharacters( cmd.substring(5), finish );
+        } else {
+          display( msg.tall.arg );
+        }
+
+      } else {
+        display( msg.help );
+      }
+  }
+}
+
+app.get('/', (req, res) => res.send( msg.hello ) );
