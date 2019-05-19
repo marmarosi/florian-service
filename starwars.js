8b41c076b9a532d9a4307050f4744e829fa00978
Mármarosi József <marmarosi@logikum.hu>
2019-05-19 16:09:41 +0200

    Add azure deployment info.

--- starwars.js
+++ starwars.js
@@ -6,7 +6,7 @@ const html = require('./html.js');
 const msg = require('./messages.js');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const COMMAND = {
   CHARS: 'characters',
